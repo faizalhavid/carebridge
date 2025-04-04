10b10482bfb3d8130b4617c25d7970c9dc82e63d
faizalhavid <nurhavid123@gmail.com>
2025-04-04 22:29:54 +0700

    feat: add Customer, CustomerRelation, BloodGroup, and Biodata models with JSON serialization; update pubspec.yaml dependencies

--- carebridge-mobile/model/lib/customer/Customer.dart
+++ carebridge-mobile/model/lib/customer/Customer.dart
@@ -1,36 +1,35 @@
+import 'dart:ffi';
+
 import 'package:copy_with_extension/copy_with_extension.dart';
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
+import 'package:model/Biodata/Biodata.dart';
+import 'package:model/BloodGroup/BloodGroup.dart';
+import 'package:model/Customer/CustomerMember.dart';
 
-part 'customer.g.dart';
+part 'Customer.g.dart';
 
 @JsonSerializable()
 @CopyWith()
 class Customer extends Equatable {
   final int id;
-  final String fullName;
-  final String fullAddress;
-  final String? secondFullAddress;
-  final String postalCode;
-  final String phone;
-  final String noIdentification;
-  final String userId;
-  final String? avatar;
-  @JsonKey(name: 'avatarView')
-  final String? avatarView;
+  final Biodata biodata;
+  final String gender;
+  final BloodGroup bloodGroup;
+  final String rhesusType;
+  final Float height;
+  final Float weight;
+  final List<CustomerMember> customerMember;
 
-  const Customer({
-    required this.id,
-    required this.fullName,
-    required this.fullAddress,
-    this.secondFullAddress,
-    required this.postalCode,
-    required this.phone,
-    required this.noIdentification,
-    required this.userId,
-    required this.avatar,
-    required this.avatarView,
-  });
+  const Customer(
+      {required this.id,
+      required this.biodata,
+      required this.gender,
+      required this.bloodGroup,
+      required this.rhesusType,
+      required this.height,
+      required this.weight,
+      required this.customerMember});
 
   factory Customer.fromJson(Map<String, dynamic> json) =>
       _$CustomerFromJson(json);
@@ -40,14 +39,13 @@ class Customer extends Equatable {
   @override
   List<Object?> get props => [
         id,
-        fullName,
-        fullAddress,
-        secondFullAddress,
-        postalCode,
-        phone,
-        noIdentification,
-        userId,
-        avatar,
-        avatarView,
+        biodata,
+        gender,
+        bloodGroup,
+        rhesusType,
+        rhesusType,
+        height,
+        weight,
+        customerMember,
       ];
 }
