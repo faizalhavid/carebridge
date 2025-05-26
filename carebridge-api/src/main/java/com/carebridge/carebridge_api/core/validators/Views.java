57f5374e497b332b0d193da7b109ae6cd33f39af
faizalhavid <nurhavid123@gmail.com>
2025-05-26 19:33:32 +0700

    feat: Add projection interfaces for user and customer data; implement UserProjection and AdminProjection with relevant fields

---
+++ carebridge-api/src/main/java/com/carebridge/carebridge_api/core/validators/Views.java
@@ -0,0 +1,9 @@
+package com.carebridge.carebridge_api.core.validators;
+
+public class Views {
+    public static class Public {
+    }
+
+    public static class Internal extends Public {
+    }
+}
